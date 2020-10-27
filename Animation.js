11f1ae38a3c4d3133eaadae4fb13e999fda19d8c
Diego Rojas <diegorojas431@gmail.com>
2020-10-26 23:14:31 -0500

    Obteniendo la data

--- Animation.js
+++ Animation.js
@@ -1,38 +1,87 @@
-// (async function load (){
-
-// 	async function getData(url) {
-// 		const response = await fetch(url);
-// 		const data = await response.json();
-// 		return data
-// 	}
-
-// 	const info = await getData ('https://xkcd.com/info.0.json');
-
-// 	console.log(info)
-// })();
-
-
 
 // https://stephencharlesweiss.com/building-xkcd-daily-digest/#footnotes
 
-function XKCDGallery() {
+// const proxyUrl = 'https://cors-anywhere.herokuapp.com'
+
+// const proxiedRequest = (url, options = {}) =>
+//   fetch(`${proxyUrl}/${url}`, {
+//     ...options,
+//     headers: {
+//       ...options.headers,
+//       'X-Requested-With': 'wololo',
+//     },
+//   })
+//   .then(resp => resp.json())
+//   .then(res => console.log({comic: res}))
+//   .catch(error => console.error(`oh no --> `, error))
+
+// const proxiedGet = url => proxiedRequest(url)
+
+// proxiedGet('https://xkcd.com/info.0.json')
+
+
+const editHtml = (res) => {
+  console.log('data: ', res);
+
+  const $section = document.getElementsByTagName('section')
+
+  function EditSection (data){
+    return (`
+      <div class="title">
+        <h2>${data.title}</h2>
+      </div>
+
+      <div class="image">
+        <img src="${data.img}" alt="${data.alt}">
+      </div>
+
+      <div class="Calification">
+        clasifica este trabajo
+
+        <ul>
+          <li><i class="fas fa-star"></i></li>
+          <li><i class="fas fa-star"></i></li>
+          <li><i class="fas fa-star"></i></li>
+          <li><i class="fas fa-star"></i></li>
+          <li><i class="fas fa-star"></i></li>
+        </ul>
+      </div>
+    `)
+  }
+
+  const HTML = EditSection(res);
+
+  console.log('html: ', HTML);
+  
+  const $home = document.implementation.createHTMLDocument()
+  
+  $home.body.innerHTML = HTML;
+  console.log('html: ', $home);
+  
+}
+
+(async function load() {
+
   const proxyUrl = 'https://cors-anywhere.herokuapp.com'
 
-  const proxiedRequest = (url, options = {}) =>
+  const proxiedRequest = async(url, options = {}) =>
     fetch(`${proxyUrl}/${url}`, {
       ...options,
-      headers: {
-        ...options.headers,
-        'X-Requested-With': 'wololo',
-      },
+        headers: {
+          ...options.headers,
+          'X-Requested-With': 'Diego',
+        },
     })
     .then(resp => resp.json())
-    .then(res => console.log({comic: res}))
-    .catch(error => console.error(`oh no --> `, error))
+    .then(res => editHtml(res))
+    .catch(error => console.error(`Tengo un error --> `, error))
 
   const proxiedGet = url => proxiedRequest(url)
 
   proxiedGet('https://xkcd.com/info.0.json')
 
-  return ()
-}
+})()
+
+
+
+
